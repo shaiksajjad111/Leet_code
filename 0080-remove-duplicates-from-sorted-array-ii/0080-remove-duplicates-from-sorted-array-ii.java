b0f7b61192ca2dd2e44a3c56cdd9c2ad0d29d43e
Shaik Sajjad Ahmed <157606819+shaiksajjad111@users.noreply.github.com>
2024-07-23 11:15:28 +0530

    Time: 32 ms (7.23%), Space: 44.1 MB (86.1%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count=0;
+        int j=1;
+        int res[]=new int[nums.length+1];
+        Arrays.fill(res,-999);
+        res[0]=nums[0];
+        for(int index=1;index<nums.length;index++){
+            int x=nums[index];
+            count=0;
+            for(int i=0;i<nums.length;i++){
+                if(x==res[i]){
+                    count++;
+                }
+            }
+            if(count<=1){
+                res[j]=x;
+                j++;
+            }
+        }
+        System.out.println("***");
+        for(int x:res){
+            System.out.println(x);  
+            }
+        System.out.println("***");
+        int z=1;
+        Arrays.fill(nums,-999);
+        nums[0]=res[0];
+        count=1;
+        for(int x=1;x<nums.length;x++){
+            if(res[x]!=-999){
+                nums[z]=res[x];
+                z++;
+                count++;
+            }
+        }
+        return count;
+        
+    }
+}
