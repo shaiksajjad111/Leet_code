d8aaabb5207faae9ce3d16e6db056b4e8d283768
Shaik Sajjad Ahmed <157606819+shaiksajjad111@users.noreply.github.com>
2024-07-25 14:00:23 +0530

    Time: 1 ms (90.16%), Space: 45.4 MB (88.13%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int maxprofit=0;
+        for(int i=0;i<prices.length-1;i++){
+            if(prices[i]<prices[i+1]){
+                maxprofit=maxprofit+(prices[i+1]-prices[i]);
+            }
+        }
+        return maxprofit;
+    }
+}
